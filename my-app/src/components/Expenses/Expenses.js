6bb35a63795c9f9fff47ca6757db1b67d350cd95
Hiroki-Ishimaru <maru.gooooooo@gmail.com>
2021-08-13 20:55:14 +0900

    dynamic lists with map

--- my-app/src/components/Expenses/Expenses.js
+++ my-app/src/components/Expenses/Expenses.js
@@ -13,21 +13,8 @@ function Expenses(props) {
 
         <Card className={'expenses'}>
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-            <ExpenseItem
-                title={props.data[0].title}
-                amount={props.data[0].amount}
-                date={props.data[0].date}
-            />
-            <ExpenseItem
-                title={props.data[1].title}
-                amount={props.data[1].amount}
-                date={props.data[1].date}
-            />
-            <ExpenseItem
-                title={props.data[2].title}
-                amount={props.data[2].amount}
-                date={props.data[2].date}
-            />
+            {props.data.map(expense => <ExpenseItem title={expense.title} amount={expense.amount}
+                                                    date={expense.date}/>)}
         </Card></div>
 }
 
